bcf5ca4a4be6897cb2bd34a271821989e83fea9b
si8168 <seanoailaoa@gmail.com>
2026-03-24 04:31:02 +1300

    style: script - videos and images excluded due to size

---
+++ js/main.js
@@ -0,0 +1,8 @@
+/* WHISKY VAULT | MASTER ARCHIVE SCRIPT 
+   ------------------------------------
+   Global interactions are currently localized in base.html 
+   to prevent conflict with the Cinematic Ad Logic.
+*/
+
+// Future Global AJAX or Animation logic goes here.
+console.log("Whisky Main Archive Script: Standby Mode.");
