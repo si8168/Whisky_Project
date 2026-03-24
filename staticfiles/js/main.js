490fa6f75b13e63932b422acf1a35a82e6831a66
Seanoa <seanoailaoa@gmail.com>
2026-03-24 14:50:54 +1300

    final: gathered static assets and updated branding

---
+++ staticfiles/js/main.js
@@ -0,0 +1,12 @@
+/* WHISKY VAULT | MASTER ARCHIVE SCRIPT 
+   ------------------------------------
+   Global interactions are currently localized in base.html 
+   to prevent conflict with the Cinematic Ad Logic.
+*/
+
+// Future Global AJAX or Animation logic goes here.
+console.log("Whisky Main Archive Script: Standby Mode.");
+
+
+// Adjusting my javascript so I can see the change in my terminal when
+// I use a git command and therefore fulfil my assessment task of 1.5! 
